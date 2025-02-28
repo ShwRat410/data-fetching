1b9b866706bb5512261f5af14e63f028a556c293
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-28 18:20:48 +0000

    Replacing fetch.then() to fetch async await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -7,14 +7,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availabelPlaces,setAvailabelPlaces] = useState([]);
 
   useEffect(()=>{
-    
-    fetch('http://localhost:3000/places').then((response)=>{
-      return response.json()
-      .then((resData)=>{
-        console.log(resData.places)
-        setAvailabelPlaces(resData.places)
-      })
-    })
+    async function fetchPlaces(){
+      const response = await fetch('http://localhost:3000/places')
+      const resData = await response.json()
+      setAvailabelPlaces(resData.places)
+    }
+    fetchPlaces()
+    // fetch('http://localhost:3000/places').then((response)=>{
+    //   return response.json()
+    //   .then((resData)=>{
+    //     console.log(resData.places)
+    //     setAvailabelPlaces(resData.places)
+    //   })
+    // })
   },[])
 
   return (
