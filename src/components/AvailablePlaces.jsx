3691a4837fcbd0cc46fdff3efcd5dfe52e6b34b3
ShwRat410 <shwetaratnawat410@gmail.com>
2025-02-28 18:17:22 +0000

    Sending http request using fetch.then() and useEffect

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
+import { useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [availabelPlaces,setAvailabelPlaces] = useState([]);
+
+  useEffect(()=>{
+    
+    fetch('http://localhost:3000/places').then((response)=>{
+      return response.json()
+      .then((resData)=>{
+        console.log(resData.places)
+        setAvailabelPlaces(resData.places)
+      })
+    })
+  },[])
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availabelPlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
